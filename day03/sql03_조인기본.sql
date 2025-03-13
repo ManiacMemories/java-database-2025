fbdcffd9045834dd4ac6052d0ebab3ae54f697a0
Kim junghyen <kimjunghyen990521@gmail.com>
2025-03-13 17:08:18 +0900

    3일차 커밋

--- day03/sql03_조인기본.sql
+++ day03/sql03_조인기본.sql
@@ -58,3 +58,50 @@ SELECT e.employee_id, e.first_name || ' ' || e.last_name AS "full_name"
  WHERE e.DEPARTMENT_ID = d.DEPARTMENT_ID
    AND l.location_id = d.location_id
    AND d.DEPARTMENT_ID = 60;
+
+-- 외부조인
+-- 보통 PK와 FK간의 일치하는 조건의 데이터를 찾는것 - 내부조인
+-- PK와 FK간의 일치하지 않는 조건의 데이터도 찾는것 - 외부조인
+-- 테이블1 OUTER JOIN 테이블2
+-- 테이블1번을 기준으로 외부조인 LEFT OUTER JOIN
+-- 테이블2번을 기준으로 외부조인 RIGHT OUTER JOIN
+
+-- ANSI 표준문법
+-- employees 테이블에는 있는데 departments테이블에는 없는 데이터를 같이 출력
+SELECT *
+  FROM employees e
+  LEFT  OUTER JOIN departments d
+ 	ON e.department_id = d.department_id
+ WHERE e.DEPARTMENT_ID IS NULL;
+
+-- ANSI 표준문법
+SELECT *
+  FROM employees e
+  RIGHT OUTER JOIN departments d
+    ON e.department_id = d.department_id;
+
+-- Oracle 문법
+-- (+)만족하지 않는 조건도 더 나오게 한다는 뜻
+-- LEFT OUTER JOIN
+SELECT *
+  FROM employees e, DEPARTMENTS d 
+ WHERE e.department_id = d.department_id(+);
+
+-- RIGHT OUTER JOIN
+SELECT *
+  FROM employees e, DEPARTMENTS d 
+ WHERE e.department_id = d.department_id;
+
+-- INNER JOIN은 INNER를 생략가능
+-- OUTER JOIN에만 LEFT, RIGHT존재하므로 OUTER 생략가능
+
+-- 셀프조인 : 자기자신을 두번 사용하는 조인
+SELECT e1.EMPLOYEE_ID
+	 , e1.FIRST_NAME || ' ' || e1.LAST_NAME AS "full_emp_name"
+	 , e1.JOB_ID
+	 , e1.MANAGER_ID
+	 , e2.FIRST_NAME || ' ' || e1.LAST_NAME AS "full_mng_name"
+	 , e2.JOB_ID
+  FROM employees e1, employees e2
+ WHERE e1.manager_ID = e2.employee_ID(+)
+ ORDER BY e1.MANAGER_ID;
